306f2c778b302c626d9b9f20629154cea60dded0
Roshan Roy <roshanroy1121@gmail.com>
2025-10-01 15:06:35 +0530

    passing props as arrays

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,12 +1,14 @@
 import "./Product.css";
 
-function Product({title, price = 1}) {
+function Product({title, price, features}) {
+    const list = features.map((feature) => <li>{feature}</li>);
     return(
         <div className="Product">
             <h3>{title}</h3>
             <h5>Price: {price}</h5>
+            <ul>{list}</ul>
         </div>
-    )
+    ) 
 }
 
 export default Product;
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,11 +1,13 @@
 import Product from "./Product";
 
 function ProductTab() {
+  let features = ["hi-tech", "durable", "fast"];
+  // let featuresObj = {a: "hi-tech", b: "durable", c: "fast"};
   return (
     <div>
-      <Product title = "Phone" price = {30000} />
-      <Product title = "Laptop" price = {50000} />
-      <Product title = "Pen" />
+      <Product title = "Phone" price = {30000} features = {features}/>
+      {/* <Product title = "Laptop" price = {50000} />
+      <Product title = "Pen" /> */}
     </div>
   );
 }
