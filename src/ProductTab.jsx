f92cf80a59a8acc4dd72e6a0de851e5dd7f33268
Roshan Roy <roshanroy1121@gmail.com>
2025-10-01 11:31:06 +0530

    props

--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -3,9 +3,9 @@ import Product from "./Product";
 function ProductTab() {
   return (
     <div>
-      <Product />
-      <Product />
-      <Product />
+      <Product title = "Phone" price = {30000} />
+      <Product title = "Laptop" price = {50000} />
+      <Product title = "Pen" />
     </div>
   );
 }
