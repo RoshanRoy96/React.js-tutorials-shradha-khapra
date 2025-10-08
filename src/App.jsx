d1eb5c8d4e626549c56548e149e8b4da07c9753d
Roshan Roy <roshanroy1121@gmail.com>
2025-10-08 10:30:14 +0530

    added new code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
-import { Title, Multiply } from "./Title";
-import Description from "./Description";
 import ProductTab from "./ProductTab";
-import MsgBox from "./MsgBox";
 
 function App() {
   return (
     <>
-      <MsgBox userName= "Roshan" textColor="red"/>
-      <ProductTab/>
+      <h2>Blockbuster deals | Shop Now</h2>
+      <ProductTab />
     </>
   );
 }
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,11 +1,18 @@
 import Product from "./Product";
 
 function ProductTab() {
+  let styles = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center"
+  }
   return (
-    <div>
-      <Product title = "Phone" price = {30000} />
-      <Product title = "Laptop" price = {50000} />
-      <Product title = "Pen" />
+    <div style = {styles}>
+      <Product title = "Logitech MX Master" idx = {0} />
+      <Product title = "Apple Pencil (2nd Gen)" idx = {1} />
+      <Product title = "Zebronics Zeb-transformer" idx = {2} />
+      <Product title = "Petronics Toad 23" idx = {3} />
     </div>
   );
 }
